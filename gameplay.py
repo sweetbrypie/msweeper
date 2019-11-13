1d9653307d42e7113b4844ce4083fcb326f32a49
Angelica Nguyen <anng214@berkeley.edu>
2019-11-12 17:15:18 -0800

    last commit

--- gameplay.py
+++ gameplay.py
@@ -15,7 +15,9 @@ class GameState(Enum):
 
 
 class Board:
-    """ Represents a minesweeper board with squares. """
+    """ 
+        Creates a playable board.
+    """
 
     def __init__(self, rows, cols):
         self.rows = rows
@@ -24,15 +26,14 @@ class Board:
         self.number_of_mines = 0
         self.max_mines = 10
         self.mines_coords = []
-        self.__create_squares(self.cols, self.rows)
-        self.set_mines(self.cols, self.rows, self.max_mines)
+        self.makeSquares(self.cols, self.rows)
+        self.setMines(self.cols, self.rows, self.max_mines)
 
-    def click_square(self, row, col):
+    def click(self, row, col):
         """ 
-            Click the square and click its
-            neighbors which don't have neighboring mines.
+            Clicks the square and, if the square does not contain a mine, also clicks its neighbors that do not contain mines.
         """
-        if not self.__valid_square(row, col) or self.__get_square(row, col).clicked:
+        if not self.validSq(row, col) or self.getSq(row, col).clicked:
             return
         square = self.squares[row][col]
         if self.game_state == GameState.start:
@@ -48,12 +49,12 @@ class Board:
             for neighbor in square.neighbors():
                 if not neighbor.mine:
                     if neighbor.mine_neighbors() == 0:
-                        self.click_square(neighbor.row, neighbor.col)
+                        self.click(neighbor.row, neighbor.col)
                     neighbor.clicked = True
-        if self.__win():
+        if self.winner():
             self.game_state = GameState.win
 
-    def print_board_wrapper(self, print_hook):
+    def pr_wrapper(self, print_hook):
         print("\n")
         col_print = "    "
         for i in range(0, self.cols):
@@ -65,7 +66,7 @@ class Board:
                 row_print += print_hook(square)
             print(row_print + "\n")
 
-    def print_board_hook(self, square):
+    def pr_hook(self, square):
         """
             Prints the board. If a square is clicked, 
             print the number of neighboring mines.
@@ -75,35 +76,37 @@ class Board:
             if square.mine_neighbors() == 0:
                 return " . "
             return " " + str(square.mine_neighbors()) + " "
+        elif square.flag:
+            return " f "
         return " X "
 
-    def print_board_end_hook(self, square):
+    def pr_endhook(self, square):
         if square.mine:
             return " M "
-        return self.print_board_hook(square)
+        return self.pr_hook(square)
 
-    def __win(self):
+    def winner(self):
         for row in self.squares:
             for square in row:
                 if not square.mine and not square.clicked:
                     return False
         return True
 
-    def __get_square(self, row, col):
+    def getSq(self, row, col):
         """ Return the square at the given row and column."""
         return self.squares[row][col]
 
-    def __valid_square(self, row, col):
+    def validSq(self, row, col):
         return 0 <= row < self.rows and 0 <= col < self.cols
 
-    def __create_squares(self, cols, rows):
+    def makeSquares(self, cols, rows):
         """
             Create a grid of squares of size rows by cols.
         """
         self.squares = [[Square(self, row, col, mine = False)
                         for col in range(cols)] for row in range(rows)]
 
-    def set_mines(self, row, col, mines):
+    def setMines(self, row, col, mines):
         for _ in range(mines):
             cell = get_random(row, col)
             while cell in self.mines_coords:
@@ -115,17 +118,24 @@ class Board:
             self.squares[sq_row][sq_col].mine = True
         return self.mines_coords
 
+    def flagSq(self, row, col):
+        if not self.validSq(row, col) or self.getSq(row, col).clicked:
+            return
+        square = self.squares[row][col]
+        square.flag = not square.flag
+
 
 class Square:
     """
         Represents a single square in the minesweeper board.
-        A square may have or may not have a mine, may be clicked or unclicked.
+        A square may have a mine (or not), may be clicked (or not), and may be flagged (or not).
     """
     def __init__(self, board, row, col, mine):
         self.board = board
         self.row = row
         self.col = col
         self.mine = mine
+        self.flag = False
         self.clicked = False
 
     def mine_neighbors(self):
--- msweep.py
+++ msweep.py
@@ -5,106 +5,117 @@ import time
 import random
 
 """ 
-	Command Line / Terminal Version of Minesweeper
-	- Created by Angelica Quach
+    Command Line / Terminal Version of Minesweeper
+    - Created by Angelica Quach
 """
 
 class Game:
 
-	def __init__(self):
-		self.board = Board(rows=10, cols=10)
+    def __init__(self):
+        self.board = Board(rows=10, cols=10)
 
-	def play(self):
-		self.welcome()
-		while self.board.game_state in [GameState.ongoing, GameState.start]:
-			self.board.print_board_wrapper(self.board.print_board_hook)
-			try:
-				inp = input("> ")
-				line = "".join(inp.split())
-				point = tuple(map(int, line.split(",")))
-				self.board.click_square(point[0], point[1])
-			except (IndexError, ValueError):
-				self.help()
-			except KeyboardInterrupt:
-				try:
-					sys.exit(0)
-				except SystemExit:
-					os._exit(0)
-		if self.board.game_state == GameState.lose:
-			print("\n\nOh no! Better luck next time! You hit a mine.\n")
-			print("Here's the solution!\n")
-		else:
-			print("\n\nYou didn't hit any of the mines! You win!\n")            
-		self.board.print_board_wrapper(self.board.print_board_end_hook)
+    def play(self):
+        self.welcome()
+        while self.board.game_state in [GameState.ongoing, GameState.start]:
+            self.board.pr_wrapper(self.board.pr_hook)
+            try:
+                inp = input("> ")
+                line = "".join(inp.split())
+                if line[0] == "f":
+                    point = tuple(map(int, line[1:].split(",")))
+                    self.board.flagSq(point[0], point[1])
+                else:
+                    point = tuple(map(int, line.split(",")))
+                    self.board.click(point[0], point[1])
+            except (IndexError, ValueError):
+                self.help()
+            except KeyboardInterrupt:
+                try:
+                    sys.exit(0)
+                except SystemExit:
+                    os._exit(0)
+        if self.board.game_state == GameState.lose:
+            print("\n\nOh no! Better luck next time! You hit a mine.\n")
+            print("Here's the solution!\n")
+        else:
+            print("\n\nYou didn't hit any of the mines! You win!\n")            
+        self.board.pr_wrapper(self.board.pr_endhook)
 
-	def welcome(self):
-		print("\nLet's play Minesweeper in Terminal!")
-		self.help()
-	
-	def help(self):
-		print("\nUse the following format to enter coordinates!")
-		print("> <row>, <column>")
-		print("> eg. 1, 1")
+    def welcome(self):
+        print("\nLet's play Minesweeper in Terminal!")
+        self.help()
+    
+    def help(self):
+        print("\nUse the following format to enter coordinates!")
+        print("> <row>, <column>")
+        print("> eg. 1, 1")
+        print("\nIf you'd like to flag a coordinate, use the following format!")
+        print("If you'd like to UNflag, please enter that same coordinate.")
+        print("> f <row>,<column>")
+        print("> f 1,1")
 
-	def play_again(self):
-		ask = input('Would you like to go again? (y/n): ')
-		return ask.lower() == 'y'
+    def play_again(self):
+        ask = input('Would you like to go again? (y/n): ')
+        return ask.lower() == 'y'
 
 
 class Solver:
-	def __init__(self):
-		self.game_count = 0
-		self.win_count = 0
-		self.possible_coords = []
-		self.make_choices()
+    def __init__(self):
+        self.game_count = 0
+        self.win_count = 0
+        self.possible_coords = []
+        self.make_choices()
 
-	def make_choices(self):
-		for i in range(10):
-			for j in range(10):
-				cell = (i, j)
-				self.possible_coords .append(cell)
+    def make_choices(self):
+        for i in range(10):
+            for j in range(10):
+                cell = (i, j)
+                self.possible_coords .append(cell)
 
-	def choose_next(self, round):
-		# current random strategy
-		return random.choice(self.possible_coords)
-		# implement actual strategy
+    def choose_next(self, round):
+        # current random strategy
+        return random.choice(self.possible_coords)
+        # planning to implement actual strategy after midterms
 
-	def autoplay(self):
-		t0 = time.time()
-		while self.game_count < 100000:
-			round = Board(rows=10, cols=10)
-			while round.game_state in [GameState.ongoing, GameState.start]:
-				guess = self.choose_next(round)
-				round.click_square(guess[0], guess[1])
-			if round.game_state == GameState.win:
-				self.win_count += 1
-			self.game_count += 1
-		t1 = time.time()
-		print("\nNumber of games won: " + str(self.win_count) + " out of " + str(self.game_count) + " games.")
-		print("Total time to complete the " + str(self.game_count) + "  attempts: " + str(t1-t0) + "seconds!\n")
+    def autoplay(self):
+        t0 = time.time()
+        while self.game_count < 100000:
+            round = Board(rows=10, cols=10)
+            while round.game_state in [GameState.ongoing, GameState.start]:
+                guess = self.choose_next(round)
+                round.click(guess[0], guess[1])
+            if round.game_state == GameState.win:
+                self.win_count += 1
+            self.game_count += 1
+        t1 = time.time()
+        timeTaken = int(t1-t0)
+        print("\nNumber of games won: " + str(self.win_count) + " out of " + str(self.game_count) + " games.")
+        print("Total time to complete the " + str(self.game_count) + "  attempts: " + str(timeTaken)+ " seconds!\n")
 
+def intro():
+    print("\n\n ~~~ Welcome to Minesweeper in Terminal! ~~~")
+    print("\n\nIf you would like to play, please enter 'p' or 'play'.")
+    print("If you would like for a solver to play the game for you, \nplease instead enter 's' or 'solver'.")
+    print("If you would like to exit, please enter 'q' or 'quit'.\n\n")
+
+def oops():
+    print("\nOops! Invalid option detected! Let's try again.")
 
 if __name__ == "__main__":
-	while True:
-		game = Game()
-		print("\n\n ~~~ Welcome to Minesweeper in Terminal! ~~~")
-		print("\n\nIf you would like to play, please enter 'p' or 'play'.")
-		print("If you would like for a solver to play the game for you, \nplease instead enter 's' or 'solver'.")
-		print("If you would like to exit, please enter 'q' or 'quit'.\n\n")
-		raw = input("> ")
-		if raw == 'p' or raw == 'play':
-			game.play()
-		elif raw == 's' or raw == 'solver':
-			print("\n\nThis is going to take a while since SOLVER will be\nplaying 100,000 games for you!")
-			solver = Solver()
-			solver.autoplay()
-		elif raw == 'q' or raw == 'quit':
-			break
-		else:
-			print("Oops! Invalid option detected! Let's try again.")
-			print("\n\nIf you would like to play, please enter 'p' or 'play'.")
-			print("If you would like for a solver to play the game for you, \nplease instead enter 's' or 'solver'.")
-			print("If you would like to exit, please enter 'q' or 'quit'.\n\n")
-			continue
-		if not game.play_again():
-			break
+    while True:
+        game = Game()
+        intro()
+        raw = input("> ")
+        if raw == 'p' or raw == 'play':
+            game.play()
+        elif raw == 's' or raw == 'solver':
+            print("\n\nThis is going to take a while since SOLVER will be\nplaying 100,000 games for you!")
+            solver = Solver()
+            solver.autoplay()
+        elif raw == 'q' or raw == 'quit':
+            break
+        else:
+            oops()
+            continue
+        if not game.play_again():
+            break
